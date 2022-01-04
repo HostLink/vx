67a7400ca5f4444f0fe251704d4634a01fcff768
Raymond Chong <mathsgod@yahoo.com>
2022-01-04 12:22:48 +0800

    post create

---
+++ class/Installer.php
@@ -0,0 +1,58 @@
+<?php
+
+use Composer\Script\Event;
+
+class Installer
+{
+    public static function postPackageInstall(Event $event)
+    {
+        $installer = new Installer;
+        echo "Database setup...\n";
+        $config = [];
+        $config["context"]["name"] = "_vx";
+
+        $config["modules"] = ["mathsgod/puxt-vx"];
+
+        $config["database"]["hostname"] = $installer->input("Please input the hostname: ");
+        $config["database"]["username"] = $installer->input("Please input the username: ");
+        $config["database"]["password"] = $installer->input("Please input the password: ");
+        $config["database"]["database"] = $installer->input("Please input the database: ");
+
+
+
+        $config["VX"]["jwt"]["secret"] =  $installer->generateRandomString();
+        $config["VX"]["language"]["en"] = "English";
+        $config["VX"]["language"]["zh-hk"] = "中文";
+
+
+        $var = var_export($config, true);
+        $var = str_replace("array (", "[", $var);
+        $var = str_replace(")", "]", $var);
+        file_put_contents(dirname(__DIR__) . "/puxt.config.php", "<?php\n\nreturn " . $var . ";\n");
+
+        $installer->installDB();
+    }
+
+    function installDB()
+    {
+        
+    }
+
+
+    function input(string $prompt = null): string
+    {
+        echo $prompt;
+        $handle = fopen("php://stdin", "r");
+        $output = fgets($handle);
+        return trim($output);
+    }
+
+    function generateRandomString($length = 128)
+    {
+        $string = '';
+        $string .= '0123456789';
+        $string .= 'abcdefghijklmnopqrstuvwxyz';
+        $string .= 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        return substr(str_shuffle(str_repeat($x = $string, ceil($length / strlen($x)))), 1, $length);
+    }
+}
